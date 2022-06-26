00bd44090ed133ba52694a9153f88b6b2ecb25ee
jhorvath <jhorvath@cvent.com>
2022-06-25 18:24:09 -0600

    more stuff

--- nn_scratch.py
+++ nn_scratch.py
@@ -1,9 +1,11 @@
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
+from nnfs.datasets import vertical_data
 
 nnfs.init()
 
+## construct a hidden layer of neurons, do a forward pass
 class Layer_Dense:
         def __init__(self, n_inputs, n_neurons):
                 self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -11,18 +13,42 @@ class Layer_Dense:
         def forward(self, inputs):
                 self.output = np.dot(inputs, self.weights) + self.biases
 
+## decide if neuron is activated based on ReLU function (above zero)
 class Activation_ReLU:
         def forward(self, inputs):
                 self.output = np.maximum(0, inputs)
 
+## Compute possibilities for output neurons
 class Activation_Softmax:
         def forward(self, inputs):
+                # get exponential values, but keep them small (subtract largest value from layer),
+                # range of values now between 0-1
                 exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+                # divide by sum of each row
                 probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
                 self.output = probabilities
 
+class Loss:
+        def calculate(self, output, y):
+                sample_losses = self.forward(output, y)
+                data_loss = np.mean(sample_losses)
+                return data_loss
 
-X, y = spiral_data(samples=100, classes=3)
+class Loss_CategoricalCrossentropy(Loss):
+        def forward(self, y_pred, y_true):
+                samples = len(y_pred)
+                y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+                if len(y_true.shape) == 1:
+                        correct_confidences = y_pred_clipped[range(samples), y_true]
+                elif len(y_true.shape) == 2:
+                        correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+                negative_log_likelihoods = -np.log(correct_confidences)
+                return negative_log_likelihoods
+
+
+X, y = vertical_data(samples=100, classes=3)
  
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
@@ -37,3 +63,8 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
 print(activation2.output[:5])
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss:", loss)
